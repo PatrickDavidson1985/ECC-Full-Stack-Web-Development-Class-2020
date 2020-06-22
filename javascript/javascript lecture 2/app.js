dd5c26f7d12cafb81c14fc50db32b7959b3ef6eb
PatrickDavidson1985 <66284891+PatrickDavidson1985@users.noreply.github.com>
2020-06-22 17:49:47 -0400

    folowing along in class

---
+++ javascript/javascript lecture 2/app.js
@@ -0,0 +1,8 @@
+console.log('this is a simple console log');
+console.error('this is an ERROR')
+console.warning('this is a warning')
+/*console is essentially a class object that provides us with numerous debugging functions.*/
+
+function test(){
+    console.log('test function firing');
+}
