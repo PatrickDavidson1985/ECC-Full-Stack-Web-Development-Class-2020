21043d940dd7bc15e89348f2b77d56ae655a20d4
PatrickDavidson1985 <66284891+PatrickDavidson1985@users.noreply.github.com>
2020-06-25 17:53:29 -0400

    ...

--- javascript/to do list/todo.js
+++ javascript/to do list/todo.js
@@ -1,21 +1,23 @@
 console.log('In todo.js!');
 
 //selectors
-let todoInput = document.getElementById('todoInput');
-let addTodoButton = document.querySelector('#addTodoButton');
+const todoInput = document.getElementById('todoInput');
+const addTodoButton = document.querySelector('#addTodoButton');
+const todoList = document.querySelector('#todoList');
 
 //console.log('todo input field:', todoInput);
 //console.log('add todo button:', addTodoButton);
 
 //get the todoInput value.
 
-let todoItemText = todoInput.value;
-//JS addEventListener Method: Add an event listener to html element that has been selected in js
-addTodoButton.addEventListener('click', elephantCallBackFunc);
 
-function elephantCallBackFunc() {
-    console.log ('Inside the elephant callback function!')
-}
+//JS addEventListener Method: Add an event listener to html element that has been selected in js
+//anon function: finction is not defined elsewhere in the codebase, and it call immediately within the arg,
+addTodoButton.addEventListener('click', function(){
+    let todoItemText = todoInput.value;
+    console.log(todoItemText);  
+});
+
 
 
 
