77851523cb968ba1d350aef1a61c79b8cc85a12f
PatrickDavidson1985 <66284891+PatrickDavidson1985@users.noreply.github.com>
2020-06-25 18:11:45 -0400

    ...

--- javascript/to do list/todo.js
+++ javascript/to do list/todo.js
@@ -4,20 +4,29 @@ console.log('In todo.js!');
 const todoInput = document.getElementById('todoInput');
 const addTodoButton = document.querySelector('#addTodoButton');
 const todoList = document.querySelector('#todoList');
+const todos = ['Static Test Todo'];
 
 //console.log('todo input field:', todoInput);
 //console.log('add todo button:', addTodoButton);
 
 //get the todoInput value.
-
+//example
 
 //JS addEventListener Method: Add an event listener to html element that has been selected in js
 //anon function: finction is not defined elsewhere in the codebase, and it call immediately within the arg,
 addTodoButton.addEventListener('click', function(){
+    //get the todoInput value
     let todoItemText = todoInput.value;
-    console.log(todoItemText);  
-});
+    
+    console.log(todos)
+   //push the todo input value to the todos array
+    todos.push(todoInput.value);
+    console.log(todos)
+})
+})
 
+
+//
 
 
 
