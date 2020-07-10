554d0bc93d100ea6b84c0aad9035f2bac3b9ddc9
PatrickDavidson1985 <66284891+PatrickDavidson1985@users.noreply.github.com>
2020-07-10 17:35:20 -0400

    yesterday's work

---
+++ javascript/objects-1.js
@@ -0,0 +1,23 @@
+// object literal
+
+const myCar = {
+    make: 'Mazda',
+    model: '3';
+    year: 2020,
+    color: 'black',
+    interior: 'leather',
+    automatic: true,
+    vin: undefined,
+}
+
+
+console.log(myCar);
+
+
+
+
+const myCart = {
+    shirt: '5x',
+    pants: '2x',
+    shoes: 'sz 13'
+}
