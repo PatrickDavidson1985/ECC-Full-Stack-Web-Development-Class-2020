25fbbaeffb8450dd2318449fb4dc33fddf891ef2
PatrickDavidson1985 <66284891+PatrickDavidson1985@users.noreply.github.com>
2020-08-04 17:03:39 -0400

    yesterday's class

--- javascript/cookies 7 30 20/cookies.js
+++ javascript/cookies 7 30 20/cookies.js
@@ -1,39 +1,76 @@
 window.onload = function() {
+    // create an object called item...
     var item = {
         "name":"david",
         "username":"adkinsd",
         "zipcode":"14223",
         "hometown":"tonawanda"
     };
+
+    //create an array called create
     var create = [];
+
+    // add item as the first element of create
     create.push(item);
+    //create.push(item);
+
+    // initialize an empty text string
     text = "";
+
+    // loop through all elements of "create" and draw a UL for each item
     for (i = 0; i < create.length; i++) {
-        text += "<ul>";
+        text += "<ul class='list-group list-group-horizontal'>";
         text += "<li class='list-group-item col'>" + create[i].name+ "</li>";
         text += "<li class='list-group-item col'>" + create[i].username+ "</li>";
         text += "<li class='list-group-item col'>" + create[i].zipcode+ "</li>";
         text += "<li class='list-group-item col'>" + create[i].hometown+ "</li>";
-        text += "<ul><li><a onclick='remove()'>remove</a></li></ul>";
+        text += "<li class='list-group-item col'> <a href='' onclick='remove("+i+")'>remove</a></li></ul>";
     }
     
+    // add our generated text to inner HTML of customtable
     document.getElementById('customtable').innerHTML = text;
 }
 
+function remove(num) {
 
-function add(row) {
+}
+
+function add(col) {
+
+        // need to add the col fields as an object to create...
+        create.push();
     
-
+        // loop through the create array AGAIN to draw out all the items in the create array.
      
-        text += "<ul>";
-        text += "<li class='list-group-item col'>" + row.name.value+ "</li>";
-        text += "<li class='list-group-item col'>" + row.username.value+ "</li>";
-        text += "<li class='list-group-item col'>" + row.zipcode.value+ "</li>";
-        text += "<li class='list-group-item col'>" + row.hometown.value+ "</li>";
-        text += "<ul><li><a onclick='remove()'>remove</a></li></ul>" ;
+
+        
+        text += "<ul class='list-group list-group-horizontal'>";
+        text += "<li class='list-group-item col'>" + col.name.value+ "</li>";
+        text += "<li class='list-group-item col'>" + col.username.value+ "</li>";
+        text += "<li class='list-group-item col'>" + col.zipcode.value+ "</li>";
+        text += "<li class='list-group-item col'>" + col.hometown.value+ "</li>";
+        text += "<li class='list-group-item col'><a href='' onclick='remove("+i+")'>remove</a></li></ul>" ;
         
         
     
     
     document.getElementById('customtable').innerHTML = text;
 }    
+
+fucntion remove(row) {
+    var index = array.indexOf(row);
+    if(index > -1) {
+        array.splice(index, 1);
+    }
+}
+
+
+fucntion add(col)  {
+
+    var item = {
+        "name":"",
+        "username":""
+        "zipcode":"",
+        "hometown":"",
+    }
+}
